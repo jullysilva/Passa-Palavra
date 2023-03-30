e8c13c7f1806d7107f95d4746b0d6e769b0227ec
Samuel Baker <samuelcastro.baker@gmail.com>
2023-03-29 23:22:08 -0300

    fix: init timer

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,13 @@
-function initTimer(){
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function initTimer () {
     console.log('iniciou o timer');
 
-    setTimeout(function() {
-        console.log('finalizou o timer');
-    }, 2 * 60000)
+    await timeout(1000); //TODO: aumentar tempo para 2 min = 2 * 60000ms
+
+    console.log('finalizou o timer');
 }
+
+module.exports = initTimer;
